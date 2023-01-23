83d07667393f87c3254fa420f40f4fb32d0aa416
Boris Stock <boris.stock@cinergy.ch>
2023-01-23 15:05:35 +0100

    turned it into "pugly" code... performant but ugly ;-)

--- src/System/AnimationSystem.php
+++ src/System/AnimationSystem.php
@@ -76,7 +76,35 @@ class AnimationSystem implements SystemInterface
         // check the AnimationContainerType of $animationContainer
         if ($animationContainer instanceof BaseAnimation) {
             // handle the animation
-            $this->handleSingleAnimation($animationContainer, $transform);
+            if (!$animationContainer->running) {
+                $animationContainer->requiredTicks = ($animationContainer->duration / 1000.0) * $this->ticksPerSecond;
+                $animationContainer->currentTick = 0;
+            }
+            $animationContainer->currentTick++;
+            if ($animationContainer instanceof TransformPositionAnimation) {
+                if (!$animationContainer->running) {
+                    $animationContainer->initialPosition = $transform->position->copy();
+                    $animationContainer->targetPosition = $animationContainer->initialPosition + $animationContainer->modifier;
+                    $animationContainer->running = true;
+                }
+                $newPosition = Vec3::slerp($animationContainer->initialPosition, $animationContainer->targetPosition, (1.0 / $animationContainer->requiredTicks) * $animationContainer->currentTick);
+                $transform->setPosition($newPosition);
+            } else if ($animationContainer instanceof TransformScaleAnimation) {
+                //@TODO: implement
+            } else if ($animationContainer instanceof TransformOrientationAnimation) {
+                if (!$animationContainer->running) {
+                    $animationContainer->initialOrientation = $transform->orientation->copy();
+                    $animationContainer->targetOrientation = $animationContainer->initialOrientation * $animationContainer->modifier;
+                    $animationContainer->running = true;
+                }
+                $newOrientation = Quat::slerp($animationContainer->initialOrientation, $animationContainer->targetOrientation, (1.0 / $animationContainer->requiredTicks) * $animationContainer->currentTick);
+                $transform->setOrientation($newOrientation);
+            }
+            // check if the animation is finished
+            if ($animationContainer->currentTick >= $animationContainer->requiredTicks) {
+                $animationContainer->finished = true;
+                $animationContainer->running = false;
+            }
         } else {
             $finishedAnimations = 0;
             if ($animationContainer instanceof AnimationSequence) {
@@ -107,100 +135,5 @@ class AnimationSystem implements SystemInterface
                 $animationContainer->finished = true;
             }
         }
-    }
-
-    /**
-     * Handles a single animation
-     *
-     * @param BaseAnimation $animation The animation
-     * @param Transform $transform The transform of the entity
-     * @return void
-     */
-    private function handleSingleAnimation(BaseAnimation $animation, Transform $transform): void
-    {
-        if ($animation instanceof TransformPositionAnimation) {
-            $this->handleTransformPositionAnimation($animation, $transform);
-        } else if ($animation instanceof TransformScaleAnimation) {
-            $this->handleTransformScaleAnimation($animation, $transform);
-        } else if ($animation instanceof TransformOrientationAnimation) {
-            $this->handleTransformOrientationAnimation($animation, $transform);
-        }
-    }
-
-    /**
-     * Handles the stuff for the base animation
-     *
-     * @param BaseAnimation $animation The animation
-     * @param Transform $transform The transform of the entity
-     *
-     * @return void
-     */
-    private function handleBaseAnimation(BaseAnimation $animation, Transform $transform): void
-    {
-        if (!$animation->running) {
-            $animation->requiredTicks = ($animation->duration / 1000.0) * $this->ticksPerSecond;
-            $animation->currentTick = 0;
-        }
-        $animation->currentTick++;
-    }
-
-    private function handleBaseAnimationFinalizer(BaseAnimation $animation, Transform $transform): void
-    {
-        if ($animation->currentTick >= $animation->requiredTicks) {
-            $animation->finished = true;
-            $animation->running = false;
-        }
-    }
-
-    /**
-     * Handles a TransformPositionAnimation
-     *
-     * @param TransformPositionAnimation $animation The animation
-     * @param Transform $transform The transform of the entity
-     * @return void
-     */
-    private function handleTransformPositionAnimation(TransformPositionAnimation $animation, Transform $transform): void
-    {
-        $this->handleBaseAnimation($animation, $transform);
-        if (!$animation->running) {
-            $animation->initialPosition = $transform->position->copy();
-            $animation->targetPosition = $animation->initialPosition + $animation->modifier;
-            $animation->running = true;
-        }
-        $newPosition = Vec3::slerp($animation->initialPosition, $animation->targetPosition, (1.0 / $animation->requiredTicks) * $animation->currentTick);
-        $transform->setPosition($newPosition);
-        $this->handleBaseAnimationFinalizer($animation, $transform);
-    }
-
-    /**
-     * Handles a TransformScaleAnimation
-     *
-     * @param TransformScaleAnimation $animation The animation
-     * @param Transform $transform The transform of the entity
-     * @return void
-     */
-    private function handleTransformScaleAnimation(TransformScaleAnimation $animation, Transform $transform): void
-    {
-        $this->handleBaseAnimation($animation, $transform);
-    }
-
-    /**
-     * Handles a TransformOrientationAnimation
-     *
-     * @param TransformOrientationAnimation $animation The animation
-     * @param Transform $transform The transform of the entity
-     * @return void
-     */
-    private function handleTransformOrientationAnimation(TransformOrientationAnimation $animation, Transform $transform): void
-    {
-        $this->handleBaseAnimation($animation, $transform);
-        if (!$animation->running) {
-            $animation->initialOrientation = $transform->orientation->copy();
-            $animation->targetOrientation = $animation->initialOrientation * $animation->modifier;
-            $animation->running = true;
-        }
-        $newOrientation = Quat::slerp($animation->initialOrientation, $animation->targetOrientation, (1.0 / $animation->requiredTicks) * $animation->currentTick);
-        $transform->setOrientation($newOrientation);
-        $this->handleBaseAnimationFinalizer($animation, $transform);
     }
 }
