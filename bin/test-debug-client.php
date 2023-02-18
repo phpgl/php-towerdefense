c6600fd8538671755a258f4fda484acb234ec62c
Boris Stock <b.stock@bitstorm.de>
2023-02-18 08:48:51 +0100

    working with structured JSON messages

--- bin/test-debug-client.php
+++ bin/test-debug-client.php
@@ -9,19 +9,31 @@ if (socket_connect($sock, '127.0.0.1', 8145) === false) {
 }
 
 $run = true;
+$msgCounter = 0;
 do {
+    echo "Command: ";
 
     $stdin = fopen('php://stdin', 'r');
+
     $msg = trim(fgets($stdin));
-    socket_write($sock, $msg, strlen($msg));
+    $msgJSON = json_encode([
+        'command' => $msg,
+        'request_id' => $msgCounter++
+    ]);
+
+    echo "\nRequest:\n$msgJSON\n\n";
+
+    socket_write($sock, $msgJSON, strlen($msgJSON));
 
     if (false === ($buf = socket_read($sock, 16384))) {
-        echo "socket_read() failed: reason: " . socket_strerror(socket_last_error($sock)) . "\n";
+        echo "socket_read() failed, reason: " . socket_strerror(socket_last_error($sock)) . "\n";
     }
 
-    echo "Response:\n$buf\n";
+    echo "\nResponse:\n";
+    print_r(json_decode($buf, true));
+    echo "\n";
 
-    if ($msg == 'quit') {
+    if ($msg == 'disconnect') {
         $run = false;
     }
 } while ($run);
--- src/Debug/DebugServer.php
+++ src/Debug/DebugServer.php
@@ -12,9 +12,9 @@ use VISU\Geo\Transform;
  */
 class DebugServer
 {
-    private string $address = '127.0.0.1'; // localhost
+    private string $address = "127.0.0.1"; // localhost
     private int $port = 8145; // default port
-    private object $socket; // the server socket
+    private ?object $socket = null; // the server socket
     private array $clients = []; // the client sockets
 
     /**
@@ -40,7 +40,13 @@ class DebugServer
         $this->socket = socket_create(AF_INET, SOCK_STREAM, 0);
 
         // bind the socket to the address and port
-        socket_bind($this->socket, $this->address, $this->port) or die('Could not bind to address');
+        if (!socket_bind($this->socket, $this->address, $this->port)) {
+            // close the server socket
+            socket_close($this->socket);
+            $this->socket = null;
+            echo "DebugServer: Could not bind to address " . $this->address . " on port " . $this->port;
+            return;
+        }
 
         // start listening for connections
         socket_listen($this->socket);
@@ -63,7 +69,9 @@ class DebugServer
         }
 
         // close the server socket
-        socket_close($this->socket);
+        if ($this->socket !== null) {
+            socket_close($this->socket);
+        }
     }
 
     /**
@@ -73,6 +81,10 @@ class DebugServer
      */
     public function process(EntitiesInterface $entities): void
     {
+        if ($this->socket == null) {
+            return;
+        }
+
         // check for new connections
         if ($newsock = socket_accept($this->socket)) {
             socket_set_nonblock($newsock);
@@ -85,10 +97,10 @@ class DebugServer
             return;
         }
 
-        $read = $this->clients;
+        $readingFromClients = $this->clients;
 
         // check for input
-        $changed = socket_select($read, $w, $e, 0);
+        $changed = socket_select($readingFromClients, $w, $e, 0);
 
         if (false == $changed) {
             return;
@@ -99,24 +111,25 @@ class DebugServer
         }
 
         // process input
-        foreach ($read as $k => $socketItem) {
+        foreach ($readingFromClients as $i => $socketItem) {
             if ($input = socket_read($socketItem, 16384)) {
                 $input = trim($input);
 
-                echo "client $k: $input\n";
+                echo "Client $i: $input\n";
 
                 $response = $this->handleInput($input, $entities);
-                socket_write($socketItem, $response, strlen($response));
+                $responseJSON = json_encode($response);
+                socket_write($socketItem, $responseJSON, strlen($responseJSON));
 
-                if ($input == 'quit') {
+                if (isset($response['request']) && $response['request'] == "disconnect") {
                     socket_close($socketItem);
-                    unset($this->clients[$k]);
+                    unset($this->clients[$i]);
                 }
             } else {
                 // most probably the socket was closed from the client side
                 echo "Socket read error: " . socket_strerror(socket_last_error()) . "\n";
                 socket_close($socketItem);
-                unset($this->clients[$k]);
+                unset($this->clients[$i]);
             }
         }
     }
@@ -126,30 +139,61 @@ class DebugServer
      * 
      * @param string $input 
      * @param EntitiesInterface $entities 
-     * @return string 
+     * @return array 
      */
-    private function handleInput(string $input, EntitiesInterface $entities): string
+    private function handleInput(string $input, EntitiesInterface $entities): array
     {
-        $response = '';
+        $inputMessageJSON = json_decode($input, true);
+        if ($inputMessageJSON == null) {
+            return [
+                'error' => "Invalid JSON on input"
+            ];
+        }
 
-        switch ($input) {
-            case 'quit':
-                $response = 'Bye!' . "\n";
+        if (!isset($inputMessageJSON['command'])) {
+            return [
+                'error' => "Missing command"
+            ];
+        }
+
+        if (!isset($inputMessageJSON['request_id'])) {
+            return [
+                'error' => "Missing request id for response"
+            ];
+        }
+
+        $response = [
+            'response_id' => $inputMessageJSON['request_id'],
+            'request' => $inputMessageJSON['command']
+        ];
+
+        switch ($inputMessageJSON['command']) {
+            case 'disconnect':
+                $response['response'] = "Bye!";
                 break;
-            case 'help':
-                $response = 'Available commands:' . "\n";
-                $response .= 'quit' . "\n";
-                $response .= 'help' . "\n";
-                $response .= 'list_transform_components' . "\n";
+            case 'available_commands':
+                $response['response'] = [
+                    "disconnect",
+                    "available_commands",
+                    "list_transform_components"
+                ];
                 break;
             case 'list_transform_components':
-                $response = 'Entities:' . "\n";
+                $transformComponents = [];
                 foreach ($entities->view(Transform::class) as $entity => $transform) {
-                    $response .= "Entity " . $entity . " Position: " . $transform->position->__toString() . "\n";
+                    $transformComponents[] = [
+                        'entity_id' => $entity,
+                        'position' => [
+                            'x' => $transform->position->x,
+                            'y' => $transform->position->y,
+                            'z' => $transform->position->z
+                        ]
+                    ];
                 }
+                $response['response'] = $transformComponents;
                 break;
             default:
-                $response = 'Unknown command' . "\n";
+                $response['response'] = "Unknown command";
                 break;
         }
 
