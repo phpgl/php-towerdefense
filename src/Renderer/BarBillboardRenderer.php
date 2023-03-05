351ce0d08ef3c178c38a60eb6b8f3d3f029fcf80
Boris Stock <b.stock@bitstorm.de>
2023-03-05 10:20:10 +0100

    some refactoring

--- src/Renderer/BarBillboardRenderer.php
+++ src/Renderer/BarBillboardRenderer.php
@@ -73,42 +73,61 @@ class BarBillboardRenderer
         $context->pipeline->addPass(new CallbackPass(
             // setup
             function (RenderPass $pass, RenderPipeline $pipeline, PipelineContainer $data) {
+                // writes directly to the backbuffer
                 $backbuffer = $data->get(BackbufferData::class)->target;
                 $pipeline->writes($pass, $backbuffer);
             },
             // execute
             function (PipelineContainer $data, PipelineResources $resources) {
+                // activate the backbuffer as render target
                 $backbuffer = $data->get(BackbufferData::class)->target;
                 $renderTarget = $resources->getRenderTarget($backbuffer);
                 $renderTarget->preparePass();
+
+                // activate the shader program
                 $this->shaderProgram->use();
 
+                // set the bar config static for now for testing, we will get this from the bar components later on
+                $barColor = new Vec4(0.0, 0.0, 0.0, 1.0);
+                $barWidth = 120.0;
+                $barHeight = 20.0;
+                $barProgress = 50.0; // 50%
+                $progressColor = new Vec4(0.5, 0.0, 0.0, 1.0);
+                $innerBarBorderWidth = 4.0;
+                $innerBarWidth = ($barWidth - ($innerBarBorderWidth * 2.0)) * ($barProgress / 100.0);
+                $innerBarHeight = $barHeight - ($innerBarBorderWidth * 2.0);
+
+                // set the projection matrix
                 $proj = new Mat4;
                 $proj->ortho(0, $renderTarget->width(), 0, $renderTarget->height(), -1, 1);
                 $this->shaderProgram->setUniformMat4('projection', false, $proj);
 
+                // set the model matrix of the outer bar
                 $model = new Mat4;
                 $model->translate(new Vec3($renderTarget->width() / 2.0, $renderTarget->height() / 2.0, 0));
-                $model->scale(new Vec3(120.0, 20.0, 1.0));
+                $model->scale(new Vec3($barWidth, $barHeight, 1.0));
                 $this->shaderProgram->setUniformMat4('model', false, $model);
 
-                $this->shaderProgram->setUniformVec4('bar_color', new Vec4(0.0, 0.0, 0.0, 1.0));
+                // set the bar color of the outer bar
+                $this->shaderProgram->setUniformVec4('bar_color', $barColor);
 
+                // draw the outer bar
                 glDisable(GL_DEPTH_TEST);
                 glDisable(GL_CULL_FACE);
-
                 $this->quadVAOuter->draw();
 
+                // set the model matrix of the inner bar
                 $model = new Mat4;
-                $model->translate(new Vec3(($renderTarget->width() / 2.0) - 56.0, $renderTarget->height() / 2.0, 0));
-                $model->scale(new Vec3(56.0, 12.0, 1.0));
+                $model->translate(new Vec3(($renderTarget->width() / 2.0) - $innerBarWidth, $renderTarget->height() / 2.0, 0));
+                $model->scale(new Vec3($innerBarWidth, $innerBarHeight, 1.0));
                 $this->shaderProgram->setUniformMat4('model', false, $model);
 
-                $this->shaderProgram->setUniformVec4('bar_color', new Vec4(0.5, 0.0, 0.0, 1.0));
+                // set the bar color of the inner bar
+                $this->shaderProgram->setUniformVec4('bar_color', $progressColor);
 
+                // draw the inner bar (progress)
                 glDisable(GL_DEPTH_TEST);
                 glDisable(GL_CULL_FACE);
-
                 $this->quadVAInner->draw();
             }
         ));
